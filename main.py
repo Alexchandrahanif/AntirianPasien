768f40991c4d4efd884699ea9f236756d24f90e6
Alex Chandra Hanif <alexchandra1998@gmail.com>
2023-10-22 15:29:53 +0700

    update untuk struktur tabel dan api nya

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from database import engine, SessionLocal
+from database import SessionLocal
 import models
+from datetime import datetime
+from database import engine
 from enum import Enum
 from typing import List
-from sqlalchemy.orm import joinedload
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
+
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
 class JenisKelaminEnum(str, Enum):
     laki_laki = "laki-laki"
     perempuan = "perempuan"
+
 
 class DokterBase(BaseModel):
     nama: str
     jenis_kelamin: JenisKelaminEnum
     nomor_telepon: str
     jabatan: str
-    
+
+
 class DokterResponse(BaseModel):
     id: int
     nama: str
@@ -32,6 +37,7 @@ class DokterResponse(BaseModel):
 class DokterCreate(DokterBase):
     pass
 
+
 class PasienBase(BaseModel):
     nik: int
     nama: str
@@ -39,8 +45,10 @@ class PasienBase(BaseModel):
     umur: int
     alamat: str
 
+
 class PasienCreate(PasienBase):
     pass
+
 
 class PasienResponse(BaseModel):
     id: int
@@ -50,23 +58,73 @@ class PasienResponse(BaseModel):
     umur: int
     alamat: str
 
+
+class LayananBase(BaseModel):
+    tanggal: datetime
+    keterangan: str
+    kuota: int
+    dokter_id: int
+
+
+class LayananCreate(LayananBase):
+    pass
+
+
+class LayananResponse(BaseModel):
+    id: int
+    tanggal: datetime
+    keterangan: str
+    kuota: int
+    dokter_id: int
+
+
 class AntrianBase(BaseModel):
     keluhan: str
-    dokter_id: int
+    layanan_id: int
     pasien_id: int
+
 
 class AntrianCreate(AntrianBase):
     pass
+
 
 class AntrianResponse(BaseModel):
     id: int
     nomor_antrian: int
     keluhan: str
-    dokter_id: int  
-    pasien_id: int  
-    dokter: DokterResponse 
-    pasien: PasienResponse  
+    layanan_id: int
+    pasien_id: int
+    dokter: DokterResponse
+    pasien: PasienResponse
 
+
+class ResponseModel(BaseModel):
+    status_code: int
+    message: str
+
+
+def reset_nomor_antrian_daily():
+    now = datetime.now()
+    reset_time = time(0, 0)
+    if now.time() == reset_time:
+
+        return 1
+    return None
+
+
+def get_next_nomor_antrian(db: Session):
+    nomor_antrian_harian = reset_nomor_antrian_daily()
+    if nomor_antrian_harian is not None:
+        return nomor_antrian_harian
+
+    highest_antrian = db.query(models.Antrian).order_by(
+        models.Antrian.nomor_antrian.desc()).first()
+    if highest_antrian:
+        next_nomor = highest_antrian.nomor_antrian + 1
+    else:
+        next_nomor = 1
+
+    return next_nomor
 
 
 def get_db():
@@ -76,30 +134,9 @@ def get_db():
     finally:
         db.close()
 
-
-def reset_nomor_antrian_daily():
-    now = datetime.now()
-    reset_time = time(0, 0)  
-    if now.time() == reset_time:
-       
-        return 1
-    return None 
-
-def get_next_nomor_antrian(db: Session):
-    nomor_antrian_harian = reset_nomor_antrian_daily()
-    if nomor_antrian_harian is not None:
-        return nomor_antrian_harian
-
-    highest_antrian = db.query(models.Antrian).order_by(models.Antrian.nomor_antrian.desc()).first()
-    if highest_antrian:
-        next_nomor = highest_antrian.nomor_antrian + 1
-    else:
-        next_nomor = 1
-
-    return next_nomor
-
-
 # CRUD Dokter
+
+
 @app.post("/dokter/", response_model=DokterResponse)
 def create_dokter(dokter: DokterCreate, db: Session = Depends(get_db)):
     db_dokter = models.Dokter(**dokter.dict())
@@ -108,43 +145,57 @@ def create_dokter(dokter: DokterCreate, db: Session = Depends(get_db)):
     db.refresh(db_dokter)
     return db_dokter
 
+
 @app.get("/dokter/", response_model=List[DokterResponse])
 def read_all_dokter(db: Session = Depends(get_db)):
     dokter = db.query(models.Dokter).all()
     return dokter
 
+
 @app.get("/dokter/{dokter_id}", response_model=DokterResponse)
 def read_dokter(dokter_id: int, db: Session = Depends(get_db)):
-    dokter = db.query(models.Dokter).filter(models.Dokter.id == dokter_id).first()
+    dokter = db.query(models.Dokter).filter(
+        models.Dokter.id == dokter_id).first()
     if dokter is None:
-        raise HTTPException(status_code=404, detail="Id Dokter Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Dokter Tidak Ditemukan")
     return dokter
+
 
 @app.put("/dokter/{dokter_id}", response_model=DokterResponse)
 def update_dokter(dokter_id: int, dokter_data: DokterCreate, db: Session = Depends(get_db)):
-    existing_dokter = db.query(models.Dokter).filter(models.Dokter.id == dokter_id).first()
+    existing_dokter = db.query(models.Dokter).filter(
+        models.Dokter.id == dokter_id).first()
     if existing_dokter is None:
-        raise HTTPException(status_code=404, detail="Id Dokter Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Dokter Tidak Ditemukan")
     for key, value in dokter_data.dict().items():
         setattr(existing_dokter, key, value)
     db.commit()
     db.refresh(existing_dokter)
     return existing_dokter
 
-@app.delete("/dokter/{dokter_id}", response_model=str, status_code=200)
+
+@app.delete("/dokter/{dokter_id}", response_model=ResponseModel, status_code=200)
 def delete_dokter(dokter_id: int, db: Session = Depends(get_db)):
-    dokter = db.query(models.Dokter).filter(models.Dokter.id == dokter_id).first()
+    dokter = db.query(models.Dokter).filter(
+        models.Dokter.id == dokter_id).first()
     if dokter is None:
-        raise HTTPException(status_code=404, detail="Id Dokter Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Dokter Tidak Ditemukan")
+
     db.delete(dokter)
     db.commit()
-    return "Data Dokter Berhasil Dihapus"
 
+    return {"status_code": 200, "message": "Data Dokter Berhasil Dihapus"}
 
 # CRUD Pasien
+
+
 @app.post("/pasien/", response_model=PasienResponse)
 def create_pasien(pasien: PasienCreate, db: Session = Depends(get_db)):
-    existing_pasien = db.query(models.Pasien).filter(models.Pasien.nik == pasien.nik).first()
+    existing_pasien = db.query(models.Pasien).filter(
+        models.Pasien.nik == pasien.nik).first()
     if existing_pasien:
         raise HTTPException(status_code=400, detail="NIK sudah terdaftar")
 
@@ -157,88 +208,184 @@ def create_pasien(pasien: PasienCreate, db: Session = Depends(get_db)):
 
 @app.get("/pasien/{pasien_id}", response_model=PasienResponse)
 def read_pasien(pasien_id: int, db: Session = Depends(get_db)):
-    pasien = db.query(models.Pasien).filter(models.Pasien.id == pasien_id).first()
+    pasien = db.query(models.Pasien).filter(
+        models.Pasien.id == pasien_id).first()
     if pasien is None:
-        raise HTTPException(status_code=404, detail="Id Pasien Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Pasien Tidak Ditemukan")
     return pasien
+
 
 @app.get("/pasien/", response_model=List[PasienResponse])
 def read_all_pasien(db: Session = Depends(get_db)):
     pasien = db.query(models.Pasien).all()
     return pasien
 
+
 @app.put("/pasien/{pasien_id}", response_model=PasienResponse)
 def update_pasien(pasien_id: int, pasien_data: PasienCreate, db: Session = Depends(get_db)):
-    existing_pasien = db.query(models.Pasien).filter(models.Pasien.id == pasien_id).first()
+    existing_pasien = db.query(models.Pasien).filter(
+        models.Pasien.id == pasien_id).first()
     if existing_pasien is None:
-        raise HTTPException(status_code=404, detail="Id Pasien Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Pasien Tidak Ditemukan")
     for key, value in pasien_data.dict().items():
         setattr(existing_pasien, key, value)
     db.commit()
     db.refresh(existing_pasien)
     return existing_pasien
 
-@app.delete("/pasien/{pasien_id}", response_model=str, status_code=200)
+
+@app.delete("/pasien/{pasien_id}", response_model=ResponseModel, status_code=200)
 def delete_pasien(pasien_id: int, db: Session = Depends(get_db)):
-    pasien = db.query(models.Pasien).filter(models.Pasien.id == pasien_id).first()
+    pasien = db.query(models.Pasien).filter(
+        models.Pasien.id == pasien_id).first()
     if pasien is None:
-        raise HTTPException(status_code=404, detail="Id Pasien Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Pasien Tidak Ditemukan")
     db.delete(pasien)
     db.commit()
-    return "Data Pasien Berhasil Dihapus"
+    return ResponseModel(status_code=200, message="Data Pasien Berhasil Dihapus")
+
+# CRUD Layanan
 
 
-
-# Endpoint untuk membuat antrian
-@app.post("/antrian/", response_model=AntrianResponse)
-def create_antrian(antrian: AntrianCreate, db: Session = Depends(get_db)):
-    existing_dokter = db.query(models.Dokter).filter(models.Dokter.id == antrian.dokter_id).first()
+@app.post("/layanan/", response_model=LayananResponse)
+def create_layanan(layanan: LayananCreate, db: Session = Depends(get_db)):
+    existing_dokter = db.query(models.Dokter).filter(
+        models.Dokter.id == layanan.dokter_id).first()
     if existing_dokter is None:
-        raise HTTPException(status_code=404, detail="ID Dokter tidak ditemukan")
+        raise HTTPException(
+            status_code=404, detail="ID Dokter tidak ditemukan")
 
-    existing_pasien = db.query(models.Pasien).filter(models.Pasien.id == antrian.pasien_id).first()
+    db_layanan = models.Layanan(**layanan.dict())
+    db.add(db_layanan)
+    db.commit()
+    db.refresh(db_layanan)
+    return db_layanan
+
+
+@app.get("/layanan/{layanan_id}", response_model=LayananResponse)
+def read_layanan(layanan_id: int, db: Session = Depends(get_db)):
+    layanan = db.query(models.Layanan).filter(
+        models.Layanan.id == layanan_id).first()
+    if layanan is None:
+        raise HTTPException(
+            status_code=404, detail="Id Layanan Tidak Ditemukan")
+    return layanan
+
+
+@app.get("/layanan/", response_model=List[LayananResponse])
+def read_all_layanan(db: Session = Depends(get_db)):
+    layanan = db.query(models.Layanan).all()
+    return layanan
+
+
+@app.put("/layanan/{layanan_id}", response_model=LayananResponse)
+def update_layanan(layanan_id: int, layanan_data: LayananCreate, db: Session = Depends(get_db)):
+    existing_layanan = db.query(models.Layanan).filter(
+        models.Layanan.id == layanan_id).first()
+    if existing_layanan is None:
+        raise HTTPException(
+            status_code=404, detail="Id Layanan Tidak Ditemukan")
+    for key, value in layanan_data.dict().items():
+        setattr(existing_layanan, key, value)
+    db.commit()
+    db.refresh(existing_layanan)
+    return existing_layanan
+
+
+@app.delete("/layanan/{layanan_id}", response_model=ResponseModel, status_code=200)
+def delete_layanan(layanan_id: int, db: Session = Depends(get_db)):
+    layanan = db.query(models.Layanan).filter(
+        models.Layanan.id == layanan_id).first()
+    if layanan is None:
+        raise HTTPException(
+            status_code=404, detail="Id Layanan Tidak Ditemukan")
+
+    db.delete(layanan)
+    db.commit()
+
+    return ResponseModel(status_code=200, message="Data Layanan Berhasil Dihapus")
+
+# CRUD Antrian
+
+
+@app.post("/antrian/", response_model=AntrianResponse)
+def create_antrian(antrian: AntrianBase, db: Session = Depends(get_db)):
+    existing_layanan = db.query(models.Layanan).filter(
+        models.Layanan.id == antrian.layanan_id).first()
+    if existing_layanan is None:
+        raise HTTPException(
+            status_code=404, detail="ID Layanan tidak ditemukan")
+
+    existing_pasien = db.query(models.Pasien).filter(
+        models.Pasien.id == antrian.pasien_id).first()
     if existing_pasien is None:
-        raise HTTPException(status_code=404, detail="ID Pasien tidak ditemukan")
+        raise HTTPException(
+            status_code=404, detail="ID Pasien tidak ditemukan")
+
+    # Cek apakah kuota layanan masih tersedia
+    if existing_layanan.kuota <= 0:
+        raise HTTPException(
+            status_code=400, detail="Kuota layanan telah habis")
 
     next_nomor_antrian = get_next_nomor_antrian(db)
-    db_antrian = models.Antrian(**antrian.dict(), nomor_antrian=next_nomor_antrian)
+
+    # Kurangi kuota layanan
+    existing_layanan.kuota -= 1
+
+    dokter = existing_layanan.dokter
+    db_antrian = models.Antrian(
+        **antrian.dict(), nomor_antrian=next_nomor_antrian)
+
+    db_antrian.dokter = dokter
+
     db.add(db_antrian)
     db.commit()
     db.refresh(db_antrian)
+
     return db_antrian
+
 
 @app.get("/antrian/{antrian_id}", response_model=AntrianResponse)
 def read_antrian(antrian_id: int, db: Session = Depends(get_db)):
-    antrian = db.query(models.Antrian).filter(models.Antrian.id == antrian_id).first()
+    antrian = db.query(models.Antrian).filter(
+        models.Antrian.id == antrian_id).first()
     if antrian is None:
-        raise HTTPException(status_code=404, detail="Id Antrian Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Antrian Tidak Ditemukan")
     return antrian
+
 
 @app.get("/antrian/", response_model=List[AntrianResponse])
 def read_all_antrian(db: Session = Depends(get_db)):
-    antrian = (
-        db.query(models.Antrian)
-        .options(joinedload(models.Antrian.pasien), joinedload(models.Antrian.dokter))
-        .all()
-    )
+    antrian = db.query(models.Antrian).all()
     return antrian
 
-@app.put("/antrian/{antrian_id}", response_model=AntrianResponse)
+
+@app.put("/antrian/{antrian_id}", response_model=ResponseModel)
 def update_antrian(antrian_id: int, antrian_data: AntrianCreate, db: Session = Depends(get_db)):
-    existing_antrian = db.query(models.Antrian).filter(models.Antrian.id == antrian_id).first()
+    existing_antrian = db.query(models.Antrian).filter(
+        models.Antrian.id == antrian_id).first()
     if existing_antrian is None:
-        raise HTTPException(status_code=404, detail="Id Antrian Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Antrian Tidak Ditemukan")
     for key, value in antrian_data.dict().items():
         setattr(existing_antrian, key, value)
     db.commit()
     db.refresh(existing_antrian)
-    return existing_antrian
 
-@app.delete("/antrian/{antrian_id}", response_model=str, status_code=200)
+    return ResponseModel(status_code=200, message="Data Antrian Berhasil Diperbaharui")
+
+
+@app.delete("/antrian/{antrian_id}", response_model=ResponseModel, status_code=200)
 def delete_antrian(antrian_id: int, db: Session = Depends(get_db)):
-    antrian = db.query(models.Antrian).filter(models.Antrian.id == antrian_id).first()
+    antrian = db.query(models.Antrian).filter(
+        models.Antrian.id == antrian_id).first()
     if antrian is None:
-        raise HTTPException(status_code=404, detail="Id Antrian Tidak Ditemukan")
+        raise HTTPException(
+            status_code=404, detail="Id Antrian Tidak Ditemukan")
     db.delete(antrian)
     db.commit()
-    return "Data Antrian Berhasil Dihapus"
+    return ResponseModel(status_code=200, message="Data Antrian Berhasil Dihapus")
--- models.py
+++ models.py
@@ -1,15 +1,17 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship
 from database import Base
+
 
 class Dokter(Base):
     __tablename__ = "dokter"
 
     id = Column(Integer, primary_key=True, index=True)
-    nama = Column(String) 
+    nama = Column(String)
     jenis_kelamin = Column(String)
     nomor_telepon = Column(String)
     jabatan = Column(String)
+
 
 class Pasien(Base):
     __tablename__ = "pasien"
@@ -21,17 +23,34 @@ class Pasien(Base):
     umur = Column(Integer)
     alamat = Column(String)
 
+
+Pasien.antrian = relationship("Antrian", back_populates="pasien")
+
+
+class Layanan(Base):
+    __tablename__ = "layanan"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tanggal = Column(Date)
+    keterangan = Column(String)
+    kuota = Column(Integer)
+    dokter_id = Column(Integer, ForeignKey("dokter.id"))
+
+    dokter = relationship("Dokter", back_populates="layanan")
+    antrian = relationship("Antrian", back_populates="layanan")
+
+
+Dokter.layanan = relationship("Layanan", back_populates="dokter")
+
+
 class Antrian(Base):
     __tablename__ = "antrian"
 
     id = Column(Integer, primary_key=True, index=True)
     nomor_antrian = Column(Integer, autoincrement=True)
     keluhan = Column(String)
-    dokter_id = Column(Integer, ForeignKey("dokter.id"))
+    layanan_id = Column(Integer, ForeignKey("layanan.id"))
     pasien_id = Column(Integer, ForeignKey("pasien.id"))
 
-    dokter = relationship("Dokter", back_populates="antrian")
     pasien = relationship("Pasien", back_populates="antrian")
-
-Dokter.antrian = relationship("Antrian", back_populates="dokter")
-Pasien.antrian = relationship("Antrian", back_populates="pasien")
+    layanan = relationship("Layanan", back_populates="antrian")
